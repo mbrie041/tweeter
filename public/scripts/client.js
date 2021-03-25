558e09bcb2ccdc4d83cd9459a90b636c5cd78cdc
Mike <mbrie041@gmail.com>
2021-03-25 18:50:19 +0000

    Fetch tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,48 +8,65 @@
 
 $(document).ready(function () {
 
+  //tweet submission function
   $('form').on('submit', function (event) {
     event.preventDefault();
     console.log('Button clicked, performing ajax call...');
 
-
     const data = $(this).serialize();
 
-    $.post('/tweets',data).then(function(){
-      console.log("Outputting something.")
+    $.post('/tweets', data).then(function () {
+      console.log("Outputting something.");
+      $('#tweets-container').trigger('reload');
     });
   });
+
+  //get tweet from /tweets function
+  const loadTweets = () => {
+
+    $.ajax({
+      url: "/tweets",
+      method: "GET"
+    })
+      .then(res => {
+        $('#tweets-container').empty()
+        renderTweets(res)
+      })
+  }
+
+  // causes the reload after a new tweet is posted
+  $('#tweets-container').on('reload', loadTweets).trigger('reload');
 
 
   const tweetData =
     [
-      {
-        "user": {
-          "name": "Newton",
-          "avatars": "https://i.imgur.com/73hZDYK.png",
-          "handle": "@SirIsaac"
-        },
-        "content": {
-          "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1616454275063
-      },
-      {
-        "user": {
-          "name": "Descartes",
-          "avatars": "https://i.imgur.com/nlhLi3I.png",
-          "handle": "@rd"
-        },
-        "content": {
-          "text": "Je pense , donc je suis"
-        },
-        "created_at": 1616540675064
-      }
+      // {
+      //   "user": {
+      //     "name": "Newton",
+      //     "avatars": "https://i.imgur.com/73hZDYK.png",
+      //     "handle": "@SirIsaac"
+      //   },
+      //   "content": {
+      //     "text": "If I have seen further it is by standing on the shoulders of giants"
+      //   },
+      //   "created_at": 1616454275063
+      // },
+      // {
+      //   "user": {
+      //     "name": "Descartes",
+      //     "avatars": "https://i.imgur.com/nlhLi3I.png",
+      //     "handle": "@rd"
+      //   },
+      //   "content": {
+      //     "text": "Je pense , donc je suis"
+      //   },
+      //   "created_at": 1616540675064
+      // }
     ]
 
 
   const createTweetElement = (tweetObject) => {
-    const $tweet = $(`    
+    const $tweet = $(`
     <article class="tweet-container">
     <header class="tweet-container-header">
       <div class="left-side">
@@ -75,11 +92,12 @@ $(document).ready(function () {
     return $tweet;
   }
 
+  //adds the new tweet to the page
   const renderTweets = (givenArray) => {
 
     for (let tweet of givenArray) {
       const $tweet = createTweetElement(tweet);
-      $('#tweets-container').append($tweet);
+      $('#tweets-container').prepend($tweet);
     }
   }
   renderTweets(tweetData);
